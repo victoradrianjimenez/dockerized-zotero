56164aec18ad33cd097d004a5cc9a62124f69418
Adrian Jimenez <victoradrianjimenez@gmail.com>
2024-02-20 12:02:53 -0300

    First version of docker services for the dataserver

---
+++ zotero-dataserver/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/zotero/dataserver.git
+git clone https://github.com/zotero/stream-server.git
+git clone https://github.com/zotero/tinymce-clean-server.git
+git clone https://github.com/zendframework/zf1.git
+mv -f ./zf1/library/Zend/* ./dataserver/include/Zend
+rm -r -f ./zf1
+wget https://dl.min.io/server/minio/release/linux-amd64/minio -O minio/minio
