ec9495c0c6acff2c43cd05859ca20a85004c8a37
Adrian Jimenez <victoradrianjimenez@gmail.com>
2024-02-21 08:00:44 -0300

    Refactor.

--- zotero-dataserver/init/init-aws.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-aws --endpoint-url "http://minio:9000" s3 mb s3://zotero
-aws --endpoint-url "http://minio:9000" s3 mb s3://zotero-fulltext
-aws --endpoint-url "http://localstack:4575" sns create-topic --name zotero
