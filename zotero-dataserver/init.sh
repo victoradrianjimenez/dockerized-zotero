b5557649d5c96f038c1ea1f3f614092cc4b7e5aa
Adrian Jimenez <victoradrianjimenez@gmail.com>
2024-02-21 04:33:45 -0300

    Stream server as new docker compose service.

--- zotero-dataserver/init.sh
+++ zotero-dataserver/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/zotero/dataserver.git
-git clone https://github.com/zotero/stream-server.git
+git clone https://github.com/zotero/stream-server.git ./stream-server/stream-server
 git clone https://github.com/zotero/tinymce-clean-server.git
 git clone https://github.com/zendframework/zf1.git
 mv -f ./zf1/library/Zend/* ./dataserver/include/Zend
