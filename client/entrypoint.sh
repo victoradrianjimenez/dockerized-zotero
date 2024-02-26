b2cb2a1aedb72b41f9a1656fcb27c0e6eb5c99ac
Adrian Jimenez <victoradrianjimenez@gmail.com>
2024-02-26 07:26:46 -0300

    Fix "The URI is malformed" error.

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 ROOT_DIR="/zotero"
+ZOTERO_CONFIG_FILE="$ROOT_DIR/resource/config.js"
 
 cp -r -f /app/{.,}* "$ROOT_DIR/"
 cd $ROOT_DIR
+
+# Configure
+DATASERVER_ADDRESS="$DATASERVER_PROTOCOL://$DATASERVER_HOST:$DATASERVER_PORT"
+WEBSOCKET_ADDRESS="$WEBSOCKET_PROTOCOL://$WEBSOCKET_HOST:$WEBSOCKET_PORT"
+sed -i "s#https://api.zotero.org/#$DATASERVER_ADDRESS/#g" $ZOTERO_CONFIG_FILE
+sed -i "s#wss://stream.zotero.org/#$WEBSOCKET_ADDRESS/#g" $ZOTERO_CONFIG_FILE
+sed -i "s#https://www.zotero.org/#$DATASERVER_ADDRESS/#g" $ZOTERO_CONFIG_FILE
+sed -i "s#https://zoteroproxycheck.s3.amazonaws.com/test##g" $ZOTERO_CONFIG_FILE
+
+# Install NodeJS Modules
+npm install
 
 # build
 PARAMS=""
