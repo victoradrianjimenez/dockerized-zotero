efd0cff6a0430a175ec9bdb020f6055d5a1896a5
Adrian Jimenez <victoradrianjimenez@gmail.com>
2024-03-09 08:02:55 -0300

    Move init scripts to another directory.

--- dataserver/scripts/create-user.sh
+++ dataserver/scripts/create-user.sh
@@ -17,12 +17,10 @@ fi
 
 # create user (www)
 userID=$(echo "INSERT INTO users (username, password) VALUES ('${1}', MD5('${3}')); SELECT LAST_INSERT_ID()" | $MYSQL zotero_www -s -N)
-echo "${userID}"
 echo "INSERT INTO users_email (userID, email) VALUES (${userID}, '${2}')" | $MYSQL zotero_www
 
 # create library
 libraryID=$(echo "INSERT INTO libraries (libraryType, shardID) VALUES ('user', 1); SELECT LAST_INSERT_ID()" | $MYSQL zotero_master -s -N)
-echo "${userID}"
 
 # create user and link to library
 echo "INSERT INTO users (userID, libraryID, username) VALUES (${userID}, ${libraryID}, '${1}')" | $MYSQL zotero_master
